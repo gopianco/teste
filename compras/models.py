0290525332017d1c67d6e8aed74f77588b75eb09
Giovane Pianco <gopianco@hotmail.com>
2019-05-29 18:16:19 -0300

    added model purchase, started dev methods of this model

--- compras/models.py
+++ compras/models.py
@@ -1,5 +1,20 @@
 from django.db import models
+from django.utils import timezone
+
+shippng_method = (
+    ('0414', 'SEDEX'),
+    ( '04510', 'PAC'),
+    ('0215', 'SEDEX10'),
+)
 
 class Purchase(models.Model):
-    purchase_id = models.Field.primary_key
+    purchase_id = models.AutoField(primary_key=True)
+    purchase_data = models.DateTimeField(default = timezone.now)
+    tracking_code = models.CharField(max_length = 200)
+    purchase_amount = models.CharField(max_length = 200)
+    tracking_mode = models.CharField(max_length = 6, choices=shippng_method)
+    shippng_time = models.CharField(max_length = 2)
     
+    def save_purchase(self):
+        self.save()
+        
