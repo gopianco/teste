831cc32b5680b06cdb215d85bb586d9c5eb0a0a5
Giovane Pianco <gopianco@hotmail.com>
2019-05-30 12:08:55 -0300

    Added prducts class

--- compras/admin.py
+++ compras/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Purchase, Product
+
+admin.site.register(Purchase)
+admin.site.register(Product)
+
 
 # Register your models here.
--- compras/models.py
+++ compras/models.py
@@ -9,12 +9,33 @@ shippng_method = (
 
 class Purchase(models.Model):
     purchase_id = models.AutoField(primary_key=True)
-    purchase_data = models.DateTimeField(default = timezone.now)
-    tracking_code = models.CharField(max_length = 200)
-    purchase_amount = models.CharField(max_length = 200)
+    purchase_itens = models.ForeignKey('Product', on_delete=models.CASCADE)
+    purchase_date = models.DateTimeField(default = timezone.now)
+    tracking_code = models.CharField(max_length = 40)
+    purchase_amount = models.DecimalField(max_digits=5, decimal_places=2)
     tracking_mode = models.CharField(max_length = 6, choices=shippng_method)
     shippng_time = models.CharField(max_length = 2)
     
     def save_purchase(self):
+        self.purchase_date = timezone.now
         self.save()
-        
+
+    def __str__(self):
+       return 'Compra nº ' + str(self.purchase_id)
+
+class Product(models.Model):
+    product_description = models.CharField(max_length=200)
+    product_cost = models.DecimalField(max_digits=20, decimal_places=2)
+    product_price = models.DecimalField(max_digits=20, decimal_places=2)
+    product_weight = models.DecimalField(max_digits=20, decimal_places=2)
+    product_width = models.DecimalField(max_digits=20, decimal_places=2)
+    product_length = models.DecimalField(max_digits=20, decimal_places=2)
+    product_id = models.AutoField(primary_key=True)
+    produtc_status = models.CharField(max_length = 40)
+    product_quantity = models.IntegerField()
+
+    def save_product(self):
+        self.save()
+    
+    def __str__(self):
+        return self.product_description
